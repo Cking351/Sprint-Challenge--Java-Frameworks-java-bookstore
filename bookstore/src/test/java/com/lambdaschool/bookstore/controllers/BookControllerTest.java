e81880a56f6c2c55ac2167344964d4fd8d4b87a4
Chris King <cking5836@gmail.com>
2020-08-21 12:54:19 -0400

    Working on controller test

--- bookstore/src/test/java/com/lambdaschool/bookstore/controllers/BookControllerTest.java
+++ bookstore/src/test/java/com/lambdaschool/bookstore/controllers/BookControllerTest.java
@@ -1,7 +1,10 @@
 package com.lambdaschool.bookstore.controllers;
 
 import com.lambdaschool.bookstore.BookstoreApplication;
+import com.lambdaschool.bookstore.models.Author;
 import com.lambdaschool.bookstore.models.Book;
+import com.lambdaschool.bookstore.models.Section;
+import com.lambdaschool.bookstore.models.Wrote;
 import com.lambdaschool.bookstore.services.BookService;
 import org.junit.After;
 import org.junit.Before;
@@ -18,7 +21,9 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @RunWith(SpringRunner.class)
 
@@ -48,9 +53,7 @@ public class BookControllerTest
     List<Book> bookList = new ArrayList<>();
 
     @Before
-    public void setUp() throws
-            Exception
-    {
+    public void setUp() throws Exception {
         /*****
          * The following is needed due to security being in place!
          */
@@ -62,6 +65,60 @@ public class BookControllerTest
          * Note that since we are only testing bookstore data, you only need to mock up bookstore data.
          * You do NOT need to mock up user data. You can. It is not wrong, just extra work.
          */
+        Author a1 = new Author("John", "Mitchell");
+        Author a2 = new Author("Dan", "Brown");
+        Author a3 = new Author("Jerry", "Poe");
+        Author a4 = new Author("Wells", "Teague");
+        Author a5 = new Author("George", "Gallinger");
+        Author a6 = new Author("Ian", "Stewart");
+
+        a1.setAuthorid(1);
+        a2.setAuthorid(2);
+        a3.setAuthorid(3);
+        a4.setAuthorid(4);
+        a5.setAuthorid(5);
+        a6.setAuthorid(6);
+
+
+        Section s1 = new Section("Fiction");
+        Section s2 = new Section("Technology");
+        Section s3 = new Section("Travel");
+        Section s4 = new Section("Business");
+        Section s5 = new Section("Religion");
+
+        s1.setSectionid(1);
+        s2.setSectionid(2);
+        s3.setSectionid(3);
+        s4.setSectionid(4);
+        s5.setSectionid(5);
+
+
+        Book b1 = new Book("Flatterland", "9780738206752", 2001, s1);
+        b1.getWrotes()
+                .add(new Wrote(a6, new Book()));
+        b1.setBookid(1);
+
+
+        Book b2 = new Book("Digital Fortess", "9788489367012", 2007, s1);
+        b2.getWrotes()
+                .add(new Wrote(a2, new Book()));
+        b2.setBookid(2);
+
+
+        Book b3 = new Book("The Da Vinci Code", "9780307474278", 2009, s1);
+        b3.getWrotes()
+                .add(new Wrote(a2, new Book()));
+        b3.setBookid(3);
+
+
+        Book b4 = new Book("Essentials of Finance", "1314241651234", 0, s4);
+        b4.setWrotes(wrote);
+        b4 = bookService.save(b4);
+
+
+        Book b5 = new Book("Calling Texas Home", "1885171382134", 2000, s3);
+        b5.setWrotes(wrote);
+        b5 = bookService.save(b5);
     }
 
     @After
