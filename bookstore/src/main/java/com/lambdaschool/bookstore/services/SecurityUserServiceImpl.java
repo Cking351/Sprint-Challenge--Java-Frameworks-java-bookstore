56bb8abb47100391cd5010bc3a84ceb872e6b9e9
Chris King <cking5836@gmail.com>
2020-08-21 12:28:33 -0400

    Security Added

--- bookstore/src/main/java/com/lambdaschool/bookstore/services/SecurityUserServiceImpl.java
+++ bookstore/src/main/java/com/lambdaschool/bookstore/services/SecurityUserServiceImpl.java
@@ -16,9 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
  * This implements User Details Service that allows us to authenticate a user.
  */
 @Service(value = "securityUserService")
-public class SecurityUserServiceImpl
-        implements UserDetailsService
-{
+public class SecurityUserServiceImpl implements UserDetailsService {
     /**
      * Ties this implementation to the User Repository so we can find a user in the database.
      */
